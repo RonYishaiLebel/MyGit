b154ca567f4a14cca57f63f84f2531806d05415c
RonYishaiLebel <143328429+RonYishaiLebel@users.noreply.github.com>
2023-08-27 22:17:34 +0300

    adding documentation

--- projects/boggle game/boggle.py
+++ projects/boggle game/boggle.py
@@ -1,3 +1,19 @@
+"""
+Boggle Game Main Script
+
+This script imports necessary modules to initialize and run a game of Boggle.
+It generates a Boggle board, reads a list of words from a text file,
+creates a BoggleGame instance, initializes a graphical user interface (GUI)
+for displaying the Boggle board, and runs the game.
+
+Make sure to have the required modules (BoggleBoard, boggle_board_randomizer, BoggleDisplay, BoggleGame)
+and the 'boggle_dict.txt' word list file in the same directory before running this script.
+
+Usage:
+    Run this script to start and play a game of Boggle.
+
+"""
+
 ######################################importing##############################
 
 from BoggleBoard import BoggleBoard
@@ -9,13 +25,14 @@ from BoggleGame import BoggleGame
 
 if __name__ == "__main__":
     words = []
-    # go throgh the words in the txt file
+    
+    # Load words from the txt file
     with open("boggle_dict.txt", 'r') as f:
         for line in f:
             for word in line.split():
                 words.append(word)
-
-    # create board, game and GUI and run the game
+    
+    # Create a Boggle board, game instance, GUI, and run the game
     board = BoggleBoard(bbr.randomize_board())
     game = BoggleGame(board, words)
     gd = gd.ColorBoard(game)
