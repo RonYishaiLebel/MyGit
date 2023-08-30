723032d0f0f1854fe88da47f6fb0037be482a4b1
RonYishaiLebel <143328429+RonYishaiLebel@users.noreply.github.com>
2023-08-30 12:17:40 +0300

    Update score.py

--- projects/boggle game/score.py
+++ projects/boggle game/score.py
@@ -1,12 +1,18 @@
+#########################IMPORTING#######################
 import tkinter as tk
 from typing import List, Tuple
 
+#########################COSNTANTS########################
+
+SCORE = "Score: "
+
+#########################SCORE CLASS######################
 
 class Score:
     def __init__(self, root, font: Tuple[str, int]) -> None:
         self.__score = 0
         self.__score_label = tk.Label(
-            root, text="Score: ", font=font)
+            root, text=SCORE, font=font)
 
     def get_score(self) -> tk.Label:
         """return the score"""
